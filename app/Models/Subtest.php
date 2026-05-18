50ee7f9596379abae2620d89509d6cc0c0d05081
Rafif Rais <rafifrais12@gmail.com>
2026-05-18 10:14:24 +0700

    feat:change return value on question func on subtest models

--- app/Models/Subtest.php
+++ app/Models/Subtest.php
@@ -35,6 +35,6 @@ class Subtest extends Model
      */
     public function questions()
     {
-        return $this->hasMany(Questions::class);
+        return $this->hasMany(questions::class);
     }
 }
